23f39f4d1615b41bb6f341d831940a405d34db68
vtorres96 <victorcdc96@gmail.com>
2019-02-28 19:37:26 -0300

    removendo caracter

--- login.php
+++ login.php
@@ -47,7 +47,7 @@
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
             <div class="login-panel panel panel-default">
-                ;<div class="panel-heading">
+                <div class="panel-heading">
                     <h3 class="panel-title">Digite o seu usuário e a sua senha</h3>
                 </div>
                 <div class="panel-body">
