76b5f7040a05784b9b373133e7523914373b6f08
vtorres96 <victorcdc96@gmail.com>
2019-03-03 17:01:03 -0300

    add link fon awesome

--- includes/head.php
+++ includes/head.php
@@ -13,4 +13,5 @@
     <link href="css/responsive.css" rel="stylesheet" type="text/css">
     <link href="css/animate.css" rel="stylesheet" type="text/css">
     <link href="css/estilo.css" rel="stylesheet" type="text/css" >
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
