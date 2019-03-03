5705a962ad4fe739c7f3ad3c46854b58b0bbf9e1
vtorres96 <victorcdc96@gmail.com>
2019-03-03 14:24:28 -0300

    consertando include do favicon

--- includes/head.php
+++ includes/head.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, maximum-scale=1">
     <title>D'Grano Panificadora</title>
-    <link rel="shortcut icon" type="imagem/x-icon" href="d-granos.png">
+    <link rel="shortcut icon" type="imagem/x-icon" href="/img/d-grano.png">
     <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,800italic,700italic,600italic,400italic,300italic,800,700,600' rel='stylesheet' type='text/css'>
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
