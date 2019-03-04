3c27f08df7c30f94b4581786692bb1d1ea00b353
vtorres96 <victorcdc96@gmail.com>
2019-03-04 15:12:37 -0300

    otimizando search para caso não encontre produto

--- funcs/search.php
+++ funcs/search.php
@@ -16,13 +16,21 @@
           $query = $pdo->query("SELECT * FROM produtos WHERE descricao LIKE '%".$_POST["retorno"]."%'");  
           $produto = $query->fetch(PDO::FETCH_ASSOC);
 
-          $output = '<ul class="list-group" id="xy">'; ?>
-               <li class="list-group-item">
-                    <img style="width:15%;" src="../imagem.php?id=<?= $produto["id"] ?>"><br>
-                    <?= $produto["descricao"]?><br>
-                    <strong>R$ </strong><?= number_format($produto["preco"],2) ?></strong><br>
-               </li>
-          <?php $output .= '</ul>';  
+          if($produto){
+               $output = '<ul class="list-group" id="xy">'; ?>
+                    <li class="list-group-item">
+                         <img style="width:15%;" src="../imagem.php?id=<?= $produto["id"] ?>"><br>
+                         <?= $produto["descricao"]?><br>
+                         <strong>R$ </strong><?= number_format($produto["preco"],2) ?></strong><br>
+                    </li>
+               <?php $output .= '</ul>';  
+          } else {
+               $output = '<ul class="list-group" id="xy">'; ?>
+                    <li class="list-group-item">
+                         Nenhum produto encontrado com esta descrição 
+                    </li>
+               <?php $output .= '</ul>';   
+          }
           echo $output;  
      } 
  
