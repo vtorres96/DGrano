bd8d0dd7887ea70f2623c5f5edbc280892c19292
vtorres96 <victorcdc96@gmail.com>
2019-03-04 15:07:10 -0300

    search funcionando com imagem do produto

--- funcs/search.php
+++ funcs/search.php
@@ -12,21 +12,17 @@
      require_once("../config/conn.php");
 
      if(isset($_POST["retorno"])){
-         
-          $output = '';  
+ 
           $query = $pdo->query("SELECT * FROM produtos WHERE descricao LIKE '%".$_POST["retorno"]."%'");  
           $produto = $query->fetch(PDO::FETCH_ASSOC);
 
-          $output = '<ul class="list-unstyled" id="xy">';   
-          foreach($produto as $prod)  {  
-               $output .= '<img style="width:75%;" src="../imagem.php?id=<?= $prod["id"] ?>">';
-               $output .= '<div class="caption">';
-                    $output .= '<strong>Descrição:</strong> <?= $prod["descricao"] ?><br>';
-                    $output .= '<strong>Preço: </strong>  R$ <strong style="font-size:25px;"><?= number_format($prod["preco"],2) ?></strong><br><br>';
-                    $output .= '<input type="submit" class="btn btn-primary" id="salvar" value="Adicionar ao Pedido" data-loading-text="Adicionando...">';
-               $output .= '</div>';
-          }  
-          $output .= '</ul>';  
+          $output = '<ul class="list-group" id="xy">'; ?>
+               <li class="list-group-item">
+                    <img style="width:15%;" src="../imagem.php?id=<?= $produto["id"] ?>"><br>
+                    <?= $produto["descricao"]?><br>
+                    <strong>R$ </strong><?= number_format($produto["preco"],2) ?></strong><br>
+               </li>
+          <?php $output .= '</ul>';  
           echo $output;  
      } 
  
